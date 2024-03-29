23f5e8ed1e78275d40bb4a604177b1e80a5494d8
vladyslavsheludko <vladsh2103@gmail.com>
2024-03-29 12:24:54 -0400

    hw done

---
+++ src/js/script.js
@@ -0,0 +1,40 @@
+// Task 1
+const age = 14;
+console.log(age);
+// Task 2
+const name = "Vlad";
+console.log(name);
+// Task 3
+const isStudent = true;
+console.log(isStudent);
+// task 4
+const myString = "Найкраща помста — величезний успіх.";
+console.log(myString);
+// task 5
+let myNumber = 21;
+myNumber = myNumber + 10;
+console.log(myNumber);
+// task 6
+const myNull = null;
+console.log(myNull);
+// task 7
+const userName = prompt("Як вас звати?");
+const congratsMessage = "Вітаю вас в системі!";
+console.log(congratsMessage, userName);
+// task 8
+const userOpinion = confirm("точно хочете підтвердити дію?");
+if (userOpinion) {
+  alert("Дякую за підтвердження!");
+} else {
+  alert("Ви успішно скасували дію.");
+}
+// task 9
+alert("Дія небезпечна!");
+const userConfirmation = confirm(
+  "Ви точно хочете підтвердити виконання цієї дії?"
+);
+if (userConfirmation) {
+  alert("Дія успішно підтверджена.");
+} else {
+  alert("Дію успішно скасовано.");
+}
